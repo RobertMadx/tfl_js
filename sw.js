48a35e3c701d99ed20efff535f7e2cd8c8624b7f
Robert Madden <Robertmadden.nz@gmail.com>
2020-11-15 17:42:47 +0200

    sw update

--- sw.js
+++ sw.js
@@ -1,57 +1,58 @@
-const statisCacheName='site-static-v10';
-const dynamicCacheName='site-dynamic-v10';
-const assets = [
-    '../',
-    '../index.html',
-    '../database.html',
-    '../index.js',
-    '../helper.js',
-    '../jquery-3.5.1.min.js',
-    '../style.css',
-    '../bootstrap.min.css',
-    '../bootstrap.min.css.map',
-    '../manifest.json',
-    '../bootstrap.min.js',
-    '../db.js',
-    '../favicon.ico',
-    '../icon-144x144.png',
-    '../jsstore.min.js',
-    '../jsstore.worker.min.js',
-    '../popper.min.js',
-];
+var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                            // Add URL you want to cache in this list.
+  '/tfl_js/',                     // If you have separate JS/CSS files,
+  '/tfl_js/index.html'            // add path to those files here
+]
 
-self.addEventListener('install', evt => {
-    //console.log('Service worked has been installed')
-    evt.waitUntil(
-        caches.open(statisCacheName).then(cache => {
-            console.log('Caching')
-            cache.addAll(assets);
-        })
-    )
-});
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
-self.addEventListener('activate', evt => {
-    //console.log('Service worked has been activated')
-    evt.waitUntil(
-        caches.keys().then(keys =>{
-            //console.log(keys);
-            return Promise.all(
-                keys.filter(key => key !== statisCacheName && key !== dynamicCacheName)
-                .map(key => caches.delete(key))
-            )
-        })
-    )
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
 })
 
-self.addEventListener('fetch', evt => {
-    evt.respondWith(
-        caches.match(evt.request).then(cacheRes =>{
-            return cacheRes || fetch(evt.request).then(fetchRes => {
-                return caches.open(dynamicCacheName).then(cache => {
-                    cache.put(evt.request.url, fetchRes.clone())
-                    return fetchRes;
-                })
-            });
-        })
-    )
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
 })
