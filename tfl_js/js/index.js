44e5df5ec6c4ae8ba22f01defffe0e16e6a0ea98
Robert Madden <Robertmadden.nz@gmail.com>
2020-11-15 16:11:57 +0200

    test

--- tfl_js/js/index.js
+++ tfl_js/js/index.js
@@ -1,9 +1,3 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-    // .then((reg) => console.log('service worker registered', reg))
-    // .catch((err) => console.log('service worker not registered', err))
-}
-
 window.onload = function () {
     initDb();
     loadAllSelect();
